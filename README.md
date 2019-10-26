ffed44a4b91e93330e9ac21f2ff39f59c4debde0
Md Kauser Ahmmed <mahmmed@uno.edu>
2019-10-26 01:08:50 -0500

    lecture 2 completed.

---
+++ README.md
@@ -0,0 +1,9 @@
+# lecture 2
+- edit activity_main.xml. 
+- add textview and button in a constraint layout. 
+- add onclickhandler for a button. 
+- log message on button click. 
+- Show click count on text view.
+- Add attribute like textSize, color to text view. 
+- Use sp - Scale independent pixel for text size. 
+- Use android resource string for text font color.
--- app/src/main/java/com/example/bonushomeworkmahmmed/MainActivity.java
+++ app/src/main/java/com/example/bonushomeworkmahmmed/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.bonushomeworkmahmmed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int count  = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void myClickHandler(View view){
+        Log.d("Test", "Button was clicked.");
+        TextView textViewCounter = (TextView) findViewById(R.id.textViewCounter);
+        textViewCounter.setText("Number of clicks: " + count);
+        count++;
     }
 }
