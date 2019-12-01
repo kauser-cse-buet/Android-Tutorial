3d960b1478322edfc31dbb20003bc50001a266a6
Md. Kauser Ahmmed <anik2137@gmail.com>
2019-12-01 01:42:15 -0600

    Lecture 10: Implemented swipe with tab using fragments.

--- app/src/main/java/com/example/bonushomeworkmahmmed/Lecture10Activity.java
+++ app/src/main/java/com/example/bonushomeworkmahmmed/Lecture10Activity.java
@@ -1,8 +1,16 @@
 package com.example.bonushomeworkmahmmed;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class Lecture10Activity extends AppCompatActivity {
 
@@ -10,5 +18,58 @@ public class Lecture10Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture10);
+
+        SectionPagerAdapter adapter = new SectionPagerAdapter(
+                getSupportFragmentManager()
+        );
+        ViewPager viewPager = findViewById(R.id.pager);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout= (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    private class SectionPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoreFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return getResources().getText(R.string.home_tab);
+                case 1:
+                    return getResources().getText(R.string.pizza_tab);
+                case 2:
+                    return getResources().getText(R.string.pasta_tab);
+                case 3:
+                    return getResources().getText(R.string.store_tab);
+            }
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/example/bonushomeworkmahmmed/StoreFragment.java
@@ -0,0 +1,39 @@
+package com.example.bonushomeworkmahmmed;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StoreFragment extends ListFragment {
+
+
+    public StoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores)
+        );
+        this.setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
